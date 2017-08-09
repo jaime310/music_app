1cb7dfaa223dc0e0c373d88ab71193f4ca692c66
Jaime Leon <jaime310@icloud.com>
2017-08-09 23:39:23 +0000

    scaffolds ok

---
+++ db/schema.rb
@@ -0,0 +1,24 @@
+ActiveRecord::Schema.define(version: 20170809233107) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "genre_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "genre_id"
+    t.integer  "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
